e2f5b662e6fd0fc1a5caecc50fe8dc51b0e7a4c6
Dinesh <dineshbelhekar111@gmail.com>
2026-06-15 23:25:43 +0530

    enabled cors request from frontend

--- src/main/java/com/example/IM/PT/configuration/SpringSecurityConfig.java
+++ src/main/java/com/example/IM/PT/configuration/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.IM.PT.configuration;
 
+import com.example.IM.PT.filter.JWTfilter;
 import com.example.IM.PT.service.UserDetailsServiceIMPL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 
 @Configuration
@@ -23,19 +30,48 @@ public class SpringSecurityConfig {
     @Autowired
     private UserDetailsServiceIMPL userDetailsService;
 
+    @Autowired
+    private JWTfilter jwtFilter;
+
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.authorizeHttpRequests(request -> request
-                        .requestMatchers("/user").hasRole("ADMIN")
+                       // .requestMatchers("/user").hasRole("ADMIN")
                         .requestMatchers("/plant/**").hasRole("ADMIN")
                         .requestMatchers("/department/**").hasAnyRole("ADMIN", "DEPTMANAGER")
                         .requestMatchers("/machine/**").hasAnyRole("ADMIN", "DEPTMANAGER", "OPERATOR")
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Configuration
+    public class CorsConfig {
+
+        @Bean
+        public CorsFilter corsFilter() {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true);
+            config.setAllowedOrigins(List.of("http://localhost:5173"));
+            config.setAllowedHeaders(List.of(
+                    "Authorization",
+                    "Content-Type",
+                    "Accept",
+                    "Origin",
+                    "X-Requested-With"
+            ));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            config.setMaxAge(3600L); // cache preflight for 1 hour
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", config);
+
+            return new CorsFilter(source);
+        }
     }
 
     @Autowired
