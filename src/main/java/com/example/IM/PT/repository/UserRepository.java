3a6724ff57715ca4b9d51b5dabaaed9fffeb92ef
Dinesh <dineshbelhekar111@gmail.com>
2026-06-18 11:35:24 +0530

    add query to find by employeeID and department

--- src/main/java/com/example/IM/PT/repository/UserRepository.java
+++ src/main/java/com/example/IM/PT/repository/UserRepository.java
@@ -3,9 +3,16 @@ package com.example.IM.PT.repository;
 import com.example.IM.PT.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
 
      User findByUsername(String Username);
 
      void deleteByemployeeId(String employeeId);
+
+     Optional<User> findByEmployeeId(String employeeId);
+
+     List<User> findByDepartment(String department);
 }
