01ffb206ad6aa53d7efb4bd6741f0a99eae7a142
Dinesh <dineshbelhekar111@gmail.com>
2026-06-27 12:02:09 +0530

    POJO to store employee data which is having live websocket connection

---
+++ src/main/java/com/example/IM/PT/Responce/UserSessionInfo.java
@@ -0,0 +1,32 @@
+package com.example.IM.PT.Responce;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSessionInfo {
+
+    private String userId;
+    private String username;
+    private String department;
+    private int machineStart;
+    private int machineEnd;
+    private String sessionId;
+
+    public UserSessionInfo(String userId, String username,
+                           String department,
+                           int machineStart,
+                           int machineEnd,
+                           String sessionId) {
+
+        this.userId = userId;
+        this.username = username;
+        this.department = department;
+        this.machineStart = machineStart;
+        this.machineEnd = machineEnd;
+        this.sessionId = sessionId;
+    }
+
+
+}
