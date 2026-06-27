e727abd0662806565e981ba734eeb31e0b074368
Dinesh <dineshbelhekar111@gmail.com>
2026-06-27 11:59:57 +0530

    add filter to send live data to particular employee who is connected to websocket

--- src/main/java/com/example/IM/PT/scheduler/LiveDataScheduler.java
+++ src/main/java/com/example/IM/PT/scheduler/LiveDataScheduler.java
@@ -1,21 +1,76 @@
 package com.example.IM.PT.scheduler;
 
+import com.example.IM.PT.DataCache.ConnectedUsersStore;
 import com.example.IM.PT.DataCache.MqttSubscriber;
+import com.example.IM.PT.Entity.MachineData;
+import com.example.IM.PT.Responce.MachineResponse;
+import com.example.IM.PT.Responce.UserSessionInfo;
+import com.example.IM.PT.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LiveDataScheduler {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SimpUserRegistry simpUserRegistry;
+
+    private final ConnectedUsersStore store;
     private final SimpMessagingTemplate messagingTemplate;
     private final MqttSubscriber mqttSubscriber; // your existing service
 
-    @Scheduled(fixedDelay = 1000) // push every 5 seconds
-    public void pushLiveData() {
-        var liveData = mqttSubscriber.getLiveData(); // your existing method
-        messagingTemplate.convertAndSend("/topic/messages", liveData);
+
+   @Scheduled(fixedDelay = 2000)
+    public void pushData() {
+
+        Map<String, MachineResponse> liveData =
+                mqttSubscriber.getLiveData();
+
+       for (UserSessionInfo user : store.getAllUsers()) {
+           if (user.getDepartment().equals("A")) {
+               messagingTemplate.convertAndSendToUser(
+                       user.getUsername(),
+                       "/queue/messages",
+                       liveData
+               );
+           } else {
+               Map<String, MachineResponse> filtered =
+                       liveData.entrySet()
+                               .stream()
+                               .filter(entry ->
+                                       entry.getValue().getDepartment()
+                                               .equals(user.getDepartment())
+                                               &&
+                                               Integer.parseInt(entry.getValue().getMachineId())
+                                                       >= user.getMachineStart()
+                                               &&
+                                               Integer.parseInt(entry.getValue().getMachineId())
+                                                       <= user.getMachineEnd()
+                               )
+                               .collect(Collectors.toMap(
+                                       Map.Entry::getKey,
+                                       Map.Entry::getValue
+                               ));
+
+               messagingTemplate.convertAndSendToUser(
+                       user.getUsername(),
+                       "/queue/messages",
+                       filtered
+               );
+
+           }
+       }
     }
 }
