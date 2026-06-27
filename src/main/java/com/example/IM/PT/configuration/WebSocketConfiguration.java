bf0ce3a90ba27a9b301f27c6e2ed8a03316f4003
Dinesh <dineshbelhekar111@gmail.com>
2026-06-27 12:03:57 +0530

    add jwt channel interceptor to validate websocket STOMP request using placed in connection header

--- src/main/java/com/example/IM/PT/configuration/WebSocketConfiguration.java
+++ src/main/java/com/example/IM/PT/configuration/WebSocketConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.IM.PT.configuration;
 
+import com.example.IM.PT.filter.JwtChannelInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,17 +13,31 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
 
+    private final JwtChannelInterceptor jwtChannelInterceptor;
+
+    public WebSocketConfiguration(JwtChannelInterceptor jwtChannelInterceptor) {
+        this.jwtChannelInterceptor = jwtChannelInterceptor;
+    }
+
         @Override
         public void configureMessageBroker(MessageBrokerRegistry registry) {
-            registry.enableSimpleBroker("/topic");
-            registry.setApplicationDestinationPrefixes("/app");
+            registry.enableSimpleBroker("/topic", "/queue");
+            registry.setApplicationDestinationPrefixes("/user");
         }
 
         @Override
         public void registerStompEndpoints(StompEndpointRegistry registry) {
             registry.addEndpoint("/ws")
-                    .setAllowedOrigins("http://localhost:5173")
+                    .setAllowedOriginPatterns("*")
                     .withSockJS();
         }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtChannelInterceptor);
     }
+
+    }
+
+
 
