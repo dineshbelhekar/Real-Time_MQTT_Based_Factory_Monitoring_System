58ae4ae597c21b18e93f10843ca597ee0d827b2e
Dinesh <dineshbelhekar111@gmail.com>
2026-07-05 18:13:50 +0530

    included method to save access and delete technician and machines on redis

--- src/main/java/com/example/IM/PT/DataCache/ConnectedUsersStore.java
+++ src/main/java/com/example/IM/PT/DataCache/ConnectedUsersStore.java
@@ -1,13 +1,17 @@
 package com.example.IM.PT.DataCache;
 
 import com.example.IM.PT.DTO.UserSessionInfo;
+import com.example.IM.PT.service.RedisService;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class ConnectedUsersStore {
+
+    private final RedisService redisService;
 
     private final ConcurrentHashMap<String, String> sessionToUser =
             new ConcurrentHashMap<>();
@@ -15,8 +19,13 @@ public class ConnectedUsersStore {
     private final ConcurrentHashMap<String, UserSessionInfo> userData =
             new ConcurrentHashMap<>();
 
-    private final ConcurrentHashMap<String, Boolean> technicianStatus =
-            new ConcurrentHashMap<>();
+  //  private final ConcurrentHashMap<String, Boolean> technicianStatus =
+  //          new ConcurrentHashMap<>();
+
+    public ConnectedUsersStore(RedisService redisService) {
+        this.redisService = redisService;
+    }
+
 
     public void add(String sessionId, UserSessionInfo info) {
 
@@ -28,7 +37,6 @@ public class ConnectedUsersStore {
     public void removeBySession(String sessionId) {
 
         String username = sessionToUser.remove(sessionId);
-
         if (username != null) {
             userData.remove(username);
         }
@@ -40,20 +48,34 @@ public class ConnectedUsersStore {
 
     public void addTechnician(String username, String sessionId){
         sessionToUser.put(sessionId,username);
-        technicianStatus.put(username,true);
+//        if(!technicianStatus.containsKey(username)){
+//            technicianStatus.put(username,true);
+//        }
+        Boolean status = redisService.getTechnicianStatus(username);
+        if (status == null || status){
+            redisService.saveTechnicianStatus(username,true);
+        }
+
     }
 
     public void removeTechnicianBySession(String sessionId){
 
-        String username = sessionToUser.remove(sessionId);
+        System.out.println(sessionId);
+        String username = sessionToUser.get(sessionId);
+//        if (username != null && technicianStatus.get(username)) {
+//            technicianStatus.remove(username);
+//        }
+        for (String str: sessionToUser.keySet()){
+            System.out.println(sessionToUser.get(str));
+        }
 
-        if (username != null) {
-            technicianStatus.remove(username);
+        if (username != null && redisService.getTechnicianStatus(username)){
+            redisService.removeTechnicianStatus(username);
         }
     }
 
-    public ConcurrentHashMap<String, Boolean> getAllTechnicians(){
-        return technicianStatus;
+    public Map<String, Boolean> getAllTechnicians(){
+        return redisService.getAllTechnicians();
     }
 
     public ConcurrentHashMap<String, String> getsessiontouser(){
@@ -61,10 +83,17 @@ public class ConnectedUsersStore {
     }
 
     public void setTechnicianStatus(String username){
-        technicianStatus.put(username,false);
+        //technicianStatus.put(username,false);
+        redisService.saveTechnicianStatus(username,false);
     }
 
     public void resetTechnicianStatus(String username){
-        technicianStatus.put(username,true);
+    //    technicianStatus.put(username,true);
+        redisService.saveTechnicianStatus(username,true);
+
+    }
+
+    public Map<String , String> getsessionuser(){
+        return sessionToUser;
     }
 }
