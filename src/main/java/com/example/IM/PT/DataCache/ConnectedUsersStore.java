d3cdefca13f09b6f8449a009876159a0020c3c72
Dinesh <dineshbelhekar111@gmail.com>
2026-07-06 09:01:40 +0530

    .

--- src/main/java/com/example/IM/PT/DataCache/ConnectedUsersStore.java
+++ src/main/java/com/example/IM/PT/DataCache/ConnectedUsersStore.java
@@ -19,9 +19,6 @@ public class ConnectedUsersStore {
     private final ConcurrentHashMap<String, UserSessionInfo> userData =
             new ConcurrentHashMap<>();
 
-  //  private final ConcurrentHashMap<String, Boolean> technicianStatus =
-  //          new ConcurrentHashMap<>();
-
     public ConnectedUsersStore(RedisService redisService) {
         this.redisService = redisService;
     }
@@ -48,11 +45,8 @@ public class ConnectedUsersStore {
 
     public void addTechnician(String username, String sessionId){
         sessionToUser.put(sessionId,username);
-//        if(!technicianStatus.containsKey(username)){
-//            technicianStatus.put(username,true);
-//        }
         Boolean status = redisService.getTechnicianStatus(username);
-        if (status == null || status){
+        if (status == null){
             redisService.saveTechnicianStatus(username,true);
         }
 
@@ -60,14 +54,7 @@ public class ConnectedUsersStore {
 
     public void removeTechnicianBySession(String sessionId){
 
-        System.out.println(sessionId);
         String username = sessionToUser.get(sessionId);
-//        if (username != null && technicianStatus.get(username)) {
-//            technicianStatus.remove(username);
-//        }
-        for (String str: sessionToUser.keySet()){
-            System.out.println(sessionToUser.get(str));
-        }
 
         if (username != null && redisService.getTechnicianStatus(username)){
             redisService.removeTechnicianStatus(username);
@@ -78,22 +65,13 @@ public class ConnectedUsersStore {
         return redisService.getAllTechnicians();
     }
 
-    public ConcurrentHashMap<String, String> getsessiontouser(){
-        return sessionToUser;
-    }
-
-    public void setTechnicianStatus(String username){
-        //technicianStatus.put(username,false);
+    public void setTechnicianStatus(String username){//technicianStatus.put(username,false);
         redisService.saveTechnicianStatus(username,false);
     }
 
     public void resetTechnicianStatus(String username){
-    //    technicianStatus.put(username,true);
         redisService.saveTechnicianStatus(username,true);
 
     }
 
-    public Map<String , String> getsessionuser(){
-        return sessionToUser;
-    }
 }
